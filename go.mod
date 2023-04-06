c3dfc835cb2959e762445cf6b5102ce09c7f16cd
Khalelalsam <anasalhomseee@gmail.com>
2023-04-06 04:41:55 +0200

    ny

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module  funtemps
+module github/khalelalsam/fun
 
-go 1.19
+go 1.20
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"funtemps/conv"
+	"github/khalelalsam/fun/conv"
 	"math"
 )
 
